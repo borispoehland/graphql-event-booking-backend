8adc1cdd8efbed3d3c17dfe3905dd44f9b9f3536
Boris Pöhland <boris.poehland@gmail.com>
2020-10-06 17:36:55 +0200

    foo

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
-app.get('/', (req, res) => {
-  res.send(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphqlbackend.jxhfs.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-  );
-});
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphqlbackend.jxhfs.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
